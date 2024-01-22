8868c60f4a6364595dafae22fbcf689a74a7d54d
Oinky <firyphoenix566@gmail.com>
2024-01-22 21:16:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
-// Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+  console.error(err); 
 });
 
-bot.connect(); // Get the bot to connect to Discord
+const channelId = "1078889213015113788";
+
+function sendSoGay() {
+  const channel = bot.getChannel(channelId);
+  if (channel) {
+    channel.createMessage("so gay");
+  }
+}
+
+// Set up interval to call the function every 30 seconds
+setInterval(sendSoGay, 15000);
+
+bot.connect();
